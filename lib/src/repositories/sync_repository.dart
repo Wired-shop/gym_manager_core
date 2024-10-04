0a0ae6d23109a66e05122568fbbd545ce16b3c90
Patrick Nicolosi <patricknicolosi99@gmail.com>
2024-10-05 00:38:52 +0200

    Aggiunta chiamata a sync

---
+++ lib/src/repositories/sync_repository.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:gym_manager_core/core.dart';
+
+class FileRepository {
+  static Future<Map<String, dynamic>> sync(String remoteIP) async {
+    String url =
+        "http://${ApiService.getIstance().getIP()}:${ApiService.getIstance().getPORT()}/gyms/${ApiService.getIstance().getGymId()}/sync/$remoteIP";
+    Response response = await ApiService.getIstance().dio.post(
+          url,
+          options: Options(headers: {
+            'Authorization':
+                'Basic ${base64Encode(utf8.encode('${ApiService.getIstance().getUsername()}:${ApiService.getIstance().getPassword()}'))}'
+          }),
+        );
+    if (response.data["responseType"] == "ok") {
+      return response.data;
+    } else {
+      throw response.data;
+    }
+  }
+}
