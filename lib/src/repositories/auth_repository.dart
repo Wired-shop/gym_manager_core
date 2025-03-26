215504253f4728a657c0695b88e151e55f46bda1
Patrick Nicolosi <patricknicolosi99@gmail.com>
2025-03-26 10:04:55 +0100

    Rimossa chiamata di list degli account

--- lib/src/repositories/auth_repository.dart
+++ lib/src/repositories/auth_repository.dart
@@ -6,7 +6,7 @@ import 'package:gym_manager_core/core.dart';
 class AuthRepository {
   static Future<String> signup(Account account) async {
     String url =
-        "http://${ApiService.getInstance().getIP()}:${ApiService.getInstance().getPORT()}/gyms/auth/signup";
+        "http://${ApiService.getInstance().getIP()}:${ApiService.getInstance().getPORT()}/auth";
     Map<String, dynamic> requestBody = account.toJson();
     requestBody['gymId'] = ApiService.getInstance().getGymId();
     Response response = await ApiService.getInstance().dio.post(
@@ -21,31 +21,9 @@ class AuthRepository {
     }
   }
 
-  static Future<List<Account>> list({String? gymId, String? email}) async {
-    String url =
-        "http://${ApiService.getInstance().getIP()}:${ApiService.getInstance().getPORT()}/gyms/auth/accounts";
-    Map<String, dynamic> queryParameters = {};
-    if (gymId != null) {
-      queryParameters['gymId'] = gymId;
-    }
-    if (email != null) {
-      queryParameters['email'] = email;
-    }
-    Response response = await ApiService.getInstance().dio.get(
-          url,
-          queryParameters: queryParameters,
-        );
-    if (response.data["responseType"] == "ok") {
-      List<dynamic> accountsData = response.data["body"];
-      return accountsData.map((json) => Account.fromJson(json)).toList();
-    } else {
-      throw response.data;
-    }
-  }
-
   static Future<List<Account>> gyms() async {
     String url =
-        "http://${ApiService.getInstance().getIP()}:${ApiService.getInstance().getPORT()}/gyms/auth/gyms";
+        "http://${ApiService.getInstance().getIP()}:${ApiService.getInstance().getPORT()}/auth/gyms";
     Response response = await ApiService.getInstance().dio.get(url);
     if (response.data["responseType"] == "ok") {
       List<dynamic> accountsData = response.data["body"];
@@ -55,9 +33,9 @@ class AuthRepository {
     }
   }
 
-  static Future delete(int id) async {
+  static Future delete({required String gymId, required String email}) async {
     String url =
-        "http://${ApiService.getInstance().getIP()}:${ApiService.getInstance().getPORT()}/gyms/auth/accounts";
+        "http://${ApiService.getInstance().getIP()}:${ApiService.getInstance().getPORT()}/auth?email=$email&gymId=$gymId";
     Response response = await ApiService.getInstance().dio.delete(
           url,
           options: Options(headers: {
