23afcf012471c39b42e28e85cfb12f8646ed409d
Patrick Nicolosi <patricknicolosi99@gmail.com>
2025-04-14 16:38:11 +0200

    Bug fix

--- example/lib/example.dart
+++ example/lib/example.dart
@@ -11,12 +11,6 @@ void main(List<String> arguments) async {
   ApiService.getInstance().setEmail(email);
   ApiService.getInstance().setPassword(password);
 
-  await SyncUtils.syncTo(
-    ip: "192.168.1.36",
-    users: await UserRepository.list(),
-    subscriptions: await SubscriptionRepository.list(),
-    courses: await CourseRepository.list(),
-    shifts: await ShiftRepository.list(),
-    bookableShifts: await BookableShiftRepository.list(),
-  );
+  await BookableShiftRepository.insert(
+      [BookableShift(shiftId: 2, courseId: 2, date: DateTime.now())]);
 }
