21169f13dca1aaeddb9f74fc3e353b24eecf0446
Patrick Nicolosi <patricknicolosi99@gmail.com>
2025-05-08 19:11:08 +0200

    REMOVED end property from BackupShift model and updated JSON serialization

--- lib/src/models/backup_shift.dart
+++ lib/src/models/backup_shift.dart
@@ -3,19 +3,23 @@ import 'package:gym_manager_core/core.dart';
 class BackupShift {
   int dayOfWeek;
   TimeOfDay start;
-  TimeOfDay end;
 
   BackupShift({
     required this.dayOfWeek,
     required this.start,
-    required this.end,
   });
 
   factory BackupShift.fromJson(Map<String, dynamic> json) {
     return BackupShift(
       dayOfWeek: json['dayOfWeek'] as int,
       start: TimeOfDay.fromString(json['start']),
-      end: TimeOfDay.fromString(json['end']),
     );
+  }
+
+  toJson() {
+    return {
+      'dayOfWeek': dayOfWeek,
+      'start': start.format24Hour(),
+    };
   }
 }
