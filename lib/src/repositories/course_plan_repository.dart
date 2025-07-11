a386922aeebd8edcceecc9e98395cb44d2330603
Samuele Francesco La Venia <106587255+Samuele-Wired@users.noreply.github.com>
2025-07-11 09:36:52 +0200

    ADDED get method to CoursePlan Repository

--- lib/src/repositories/course_plan_repository.dart
+++ lib/src/repositories/course_plan_repository.dart
@@ -3,6 +3,25 @@ import 'package:dio/dio.dart';
 import 'package:gym_manager_core/core.dart';
 
 class CoursePlanRepository {
+  static Future<CoursePlan?> get(int id) async {
+    String url =
+        "https://${ApiService.getInstance().getIP()}:${ApiService.getInstance().getPORT()}/gyms/${ApiService.getInstance().getGymId()}/coursePlans/$id";
+    Response response = await ApiService.getInstance().dio.get(
+          url,
+          options: Options(headers: {
+            'Authorization':
+                'Basic ${base64Encode(utf8.encode('${ApiService.getInstance().getEmail()}:${ApiService.getInstance().getPassword()}'))}'
+          }),
+        );
+    if (response.data["responseType"] == "ok" &&
+        response.data["body"] != null) {
+      CoursePlan coursePlan = CoursePlan.fromJson(response.data["body"]);
+      return coursePlan;
+    } else {
+      return null;
+    }
+  }
+
   static Future<List<CoursePlan>> list(int courseId) async {
     String url =
         "https://${ApiService.getInstance().getIP()}:${ApiService.getInstance().getPORT()}/gyms/${ApiService.getInstance().getGymId()}/coursePlans?courseId=$courseId";
