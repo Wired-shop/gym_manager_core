67119a145d1d5e6ce1edf6665e50ceb80676b234
Patrick Nicolosi <patricknicolosi99@gmail.com>
2026-04-08 22:11:40 +0200

    Refactor EntryRejectionReasons enum to include 'sconosciuto' and update string conversion methods

--- lib/src/enums/entry_rejection_reasons.dart
+++ lib/src/enums/entry_rejection_reasons.dart
@@ -1,16 +1,28 @@
 enum EntryRejectionReasons {
   accessiGiornalieriEsauriti,
   accessiDisponibiliEsauriti,
-  fuoriFasciaOraria;
+  fuoriFasciaOraria,
+  sconosciuto;
 
-  String get label {
-    switch (this) {
+  static EntryRejectionReasons fromString(String? value) {
+    for (int i = 0; i < EntryRejectionReasons.values.length; i++) {
+      if (EntryRejectionReasons.values[i].name == value) {
+        return EntryRejectionReasons.values[i];
+      }
+    }
+    return EntryRejectionReasons.sconosciuto;
+  }
+
+  static String toPrettyString(EntryRejectionReasons entryRejectionReasons) {
+    switch (entryRejectionReasons) {
       case EntryRejectionReasons.accessiGiornalieriEsauriti:
         return "Accessi giornalieri esauriti";
       case EntryRejectionReasons.accessiDisponibiliEsauriti:
         return "Accessi disponibili esauriti";
       case EntryRejectionReasons.fuoriFasciaOraria:
         return "Fuori fascia oraria";
+      case EntryRejectionReasons.sconosciuto:
+        return "Sconosciuto";
     }
   }
 }
