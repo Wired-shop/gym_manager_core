0c769051b4109756fac2b4f9805d287f88509c5b
Patrick Nicolosi <patricknicolosi99@gmail.com>
2026-04-13 17:46:45 +0200

    Refactor init method in SyncRepository to include supabaseUrl and supabaseKey parameters

--- lib/src/repositories/sync_repository.dart
+++ lib/src/repositories/sync_repository.dart
@@ -1,6 +1,24 @@
 import '../services/api_service.dart';
 
 class SyncRepository {
+  static Future<Map<String, dynamic>> init({
+    required String supabaseUrl,
+    required String supabaseKey,
+  }) async {
+    final url =
+        "https://${ApiService.getInstance().getIP()}:${ApiService.getInstance().getPORT()}/sync/init";
+
+    final response = await ApiService.getInstance().dio.post(
+      url,
+      data: {
+        'supabaseUrl': supabaseUrl,
+        'supabaseKey': supabaseKey,
+      },
+    );
+
+    return Map<String, dynamic>.from(response.data);
+  }
+
   static Future<Map<String, dynamic>> sync({
     required String gymId,
     required String email,
