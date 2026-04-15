a461f181886d6e02ef4c7a179e8fc4980a2915aa
Patrick Nicolosi <patricknicolosi99@gmail.com>
2026-04-15 14:37:03 +0200

    Refactor init and sync methods in SyncRepository to return void instead of Map

--- lib/src/repositories/sync_repository.dart
+++ lib/src/repositories/sync_repository.dart
@@ -1,33 +1,29 @@
 import '../services/api_service.dart';
 
 class SyncRepository {
-  static Future<Map<String, dynamic>> init({
+  static Future<void> init({
     required String supabaseUrl,
     required String supabaseAnonKey,
   }) async {
     final url =
         "https://${ApiService.getInstance().getIP()}:${ApiService.getInstance().getPORT()}/sync/init";
-
-    final response = await ApiService.getInstance().dio.post(
+    await ApiService.getInstance().dio.post(
       url,
       data: {
         'supabaseUrl': supabaseUrl,
         'supabaseAnonKey': supabaseAnonKey,
       },
     );
-
-    return Map<String, dynamic>.from(response.data);
   }
 
-  static Future<Map<String, dynamic>> sync({
+  static Future<void> sync({
     required String gymId,
     required String email,
     required String password,
   }) async {
     final url =
         "https://${ApiService.getInstance().getIP()}:${ApiService.getInstance().getPORT()}/sync";
-
-    final response = await ApiService.getInstance().dio.post(
+    await ApiService.getInstance().dio.post(
       url,
       data: {
         'gymId': gymId,
@@ -35,7 +31,5 @@ class SyncRepository {
         'password': password,
       },
     );
-
-    return Map<String, dynamic>.from(response.data);
   }
 }
