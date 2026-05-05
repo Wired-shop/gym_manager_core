f4a8a5ee1cfac555fd6a839ead4ebd6db07c10ca
Patrick Nicolosi <patricknicolosi99@gmail.com>
2026-05-05 18:48:35 +0200

    Remove cancelled status from BookingStatus enum and update related methods

--- lib/src/enums/booking_status.dart
+++ lib/src/enums/booking_status.dart
@@ -1,18 +1,15 @@
 enum BookingStatus {
   confirmed,
-  cancelled,
   used;
 
   static BookingStatus fromString(String value) => switch (value) {
-    'confirmed' => BookingStatus.confirmed,
-    'cancelled' => BookingStatus.cancelled,
-    'used' => BookingStatus.used,
-    _ => throw Exception('BookingStatus sconosciuto: $value'),
-  };
+        'confirmed' => BookingStatus.confirmed,
+        'used' => BookingStatus.used,
+        _ => throw Exception('BookingStatus sconosciuto: $value'),
+      };
 
   String toJson() => switch (this) {
-    BookingStatus.confirmed => 'confirmed',
-    BookingStatus.cancelled => 'cancelled',
-    BookingStatus.used => 'used',
-  };
+        BookingStatus.confirmed => 'confirmed',
+        BookingStatus.used => 'used',
+      };
 }
